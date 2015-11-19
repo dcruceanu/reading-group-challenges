a760777df807c2b9dab8f83acf67b754849b0e2f
dcruceanu <daniela.cruceanu@cegeka.com>
2015-11-19 20:26:54 +0200

    002: Manually creating service

--- challenge-002-patterns-dependency-injection/patterns-depedency-injection-app/src/PlaygroundBundle/DependencyInjection/PlaygroundExtension.php
+++ challenge-002-patterns-dependency-injection/patterns-depedency-injection-app/src/PlaygroundBundle/DependencyInjection/PlaygroundExtension.php
@@ -6,6 +6,7 @@ namespace PlaygroundBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -19,7 +20,12 @@ class PlaygroundExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $definition = new Definition('PlaygroundBundle\Service\FloatStorage');
+        $definition->addTag('simple_storage');
+        $container->setDefinition('playground.service.float_storage', $definition);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
         $loader->load('services.yml');
         $loader->load('repositories.yml');
     }
